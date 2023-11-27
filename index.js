57532fe2e70f3fd57dde70dd35399a9d63e3890e
stephen muiru <stevembugua26@gmail.com>
2023-11-27 20:46:42 +0300

    added a simple user interface

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 console.log("hello world")
 
+let playerChoice;
+
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const results = document.querySelector('.results');
+const playAgainButton = document.querySelector('.play-again');
+
+let roundsPlayed = 0;
+let scorePlayer = 0;
+let scoreComputer = 0;
+let tieCount = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * choices.length);
@@ -20,65 +33,90 @@ function checkWinner(playerSelection, computerSelection) {
   }
 }
 
- 
 function playRound(playerSelection, computerSelection) {
   const result = checkWinner(playerSelection, computerSelection);
 
   if (result === "tie") {
-    return "It's a tie";
+    const p = document.createElement('p');
+    p.innerText = "It's a tie";
+    results.appendChild(p);
+    tieCount++;
   } else if (result === "player") {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    const p = document.createElement('p');
+    p.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+    results.appendChild(p);
+    scorePlayer++;
   } else if (result === "computer") {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    const p = document.createElement('p');
+    p.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    results.appendChild(p);
+    scoreComputer++;
   }
+  
 }
 
-function getPlayerChoice() {
-  let validatedInput = false;
-  let choice;
-
-  while (!validatedInput) {
-    const choices = ["rock", "paper", "scissors"];
-    choice = prompt("Enter your choice:").toLowerCase();
-
-    if (choices.includes(choice)) {
-      validatedInput = true;
-      return choice;
+function checkGameEnd() {
+  if (scorePlayer >= 5 || scoreComputer >= 5 || tieCount >=5 ){
+    const winnerMessage = document.createElement('h2');
+    if (scorePlayer > scoreComputer) {
+      winnerMessage.innerText = "Winner Winner Chicken Dinner";
+    } else if (scorePlayer < scoreComputer) {
+      winnerMessage.innerText = "Loser";
+    } else {
+      winnerMessage.innerText = "We have a tie";
     }
+    results.appendChild(winnerMessage);
+
+    const gameOverMessage = document.createElement('h2');
+    gameOverMessage.innerText = "Game over";
+    results.appendChild(gameOverMessage);
+
+    restartGame();
   }
+
 }
-
-
-
-function game() {
-  let scorePlayer = 0;
-  let scoreComputer = 0;
-
-  console.log("Welcome to Rock Paper Scissors");
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-
-    const result = checkWinner(playerSelection, computerSelection);
-
-    if (result === "player") {
-      scorePlayer++;
-    } else if (result === "computer") {
-      scoreComputer++;
-    }
-  }
-
-  if (scorePlayer > scoreComputer) {
-    console.log("Player wins");
-  } else if (scorePlayer < scoreComputer) {
-    console.log("Computer wins");
+function showPlayAgainButton(show) {
+  const playAgainButton = document.getElementById("playAgainButton");
+  if (show) {
+    playAgainButton.style.display = "block";
   } else {
-    console.log("We have a tie");
+    playAgainButton.style.display = "none";
   }
-
-  console.log("Game Over");
 }
 
-game();
+
+function restartGame() {
+  playerChoice = null;
+  showPlayAgainButton(true);
+  document.getElementById(".play-again").style.display = "block";
+  
+  
+}
+
+rockButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const playerChoice = 'rock';
+  playRound(playerChoice, computerSelection);
+  roundsPlayed++;
+  checkGameEnd();
+});
+
+paperButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const playerChoice = 'paper';
+  playRound(playerChoice, computerSelection);
+  roundsPlayed++;
+  checkGameEnd();
+});
+
+scissorsButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const playerChoice = 'scissors';
+  playRound(playerChoice, computerSelection);
+  roundsPlayed++;
+  checkGameEnd();
+});
+
+playAgainButton.addEventListener('click', () => {
+restartGame();
+});
